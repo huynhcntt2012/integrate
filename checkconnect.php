d4b977f8c6e7bf92cf9f839a2aa94e06222e57d6
huynhcntt2012 <huynhcntt2012@gmail.com>
2024-10-10 11:09:59 +0700

    Update checkconnect.php

--- checkconnect.php
+++ checkconnect.php
@@ -14,6 +14,20 @@ try {
 
     // Nếu kết nối thành công, in ra thông báo
     echo "Kết nối tới cơ sở dữ liệu thành công!";
+    $sql = "SHOW TABLES FROM $dbname";
+    $result = mysql_query($sql);
+    
+    if (!$result) {
+        echo "DB Error, could not list tables\n";
+        echo 'MySQL Error: ' . mysql_error();
+        exit;
+    }
+    
+    while ($row = mysql_fetch_row($result)) {
+        echo "Table: {$row[0]}\n";
+    }
+    
+    mysql_free_result($result);
 } catch (PDOException $e) {
     // Nếu có lỗi, in ra lỗi
     echo "Kết nối tới cơ sở dữ liệu thất bại: " . $e->getMessage();
