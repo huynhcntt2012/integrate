58ca16a600e9564d5d56b31f2707993daa84cb65
Huynh <huynhcntt2012@gmail.com>
2024-10-10 10:56:30 +0700

    create

---
+++ checkconnect.php
@@ -0,0 +1,21 @@
+<?php
+// Thông tin kết nối cơ sở dữ liệu
+$host = 'localhost';  // Địa chỉ của database (localhost nếu cài đặt trên cùng máy)
+$dbname = 'asteriskcdrdb'; // Tên database Asterisk
+$username = 'root';   // Tên đăng nhập của MySQL
+$password = 'fwa@865221'; // Mật khẩu của MySQL
+
+try {
+    // Tạo kết nối PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    // Thiết lập chế độ báo lỗi cho PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Nếu kết nối thành công, in ra thông báo
+    echo "Kết nối tới cơ sở dữ liệu thành công!";
+} catch (PDOException $e) {
+    // Nếu có lỗi, in ra lỗi
+    echo "Kết nối tới cơ sở dữ liệu thất bại: " . $e->getMessage();
+}
+?>
